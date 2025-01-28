e4440509fc20b21116b31b1fd57c11e4790923ce
ataerdal <ata.erdal@appcent.mobi>
2025-01-28 23:41:23 +0300

    Create core package

---
+++ app/src/main/java/com/herdal/bitcointicker/core/di/DatabaseModule.kt
@@ -0,0 +1,25 @@
+package com.herdal.bitcointicker.core.di
+
+import android.content.Context
+import androidx.room.Room
+import com.herdal.bitcointicker.core.data.local.CoinDatabase
+import com.herdal.bitcointicker.core.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@[Module InstallIn(SingletonComponent::class)]
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): CoinDatabase {
+        return Room.databaseBuilder(
+            context,
+            CoinDatabase::class.java,
+            Constants.DATABASE_NAME
+        ).build()
+    }
+}
