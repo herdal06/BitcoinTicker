2377c62f41259e401f9e65743e5cf5b5fec56d58
ataerdal <ata.erdal@appcent.mobi>
2025-01-29 18:11:09 +0300

    Create core remote

---
+++ app/src/main/java/com/herdal/bitcointicker/features/coin/data/remote/datasource/CoinRemoteDataSourceImpl.kt
@@ -0,0 +1,16 @@
+package com.herdal.bitcointicker.features.coin.data.remote.datasource
+
+import com.herdal.bitcointicker.core.data.remote.ApiExecutor
+import com.herdal.bitcointicker.core.data.remote.Result
+import com.herdal.bitcointicker.core.data.remote.NetworkHelper
+import com.herdal.bitcointicker.features.coin.data.remote.dto.CoinsResponse
+import com.herdal.bitcointicker.features.coin.data.remote.service.CoinService
+
+class CoinRemoteDataSourceImpl(
+    private val coinService: CoinService,
+    networkHelper: NetworkHelper
+) : ApiExecutor(networkHelper), CoinRemoteDataSource {
+    override suspend fun getCoins(currency: String): Result<CoinsResponse> {
+        return executeApiCall { coinService.getCoins(currency) }
+    }
+}
